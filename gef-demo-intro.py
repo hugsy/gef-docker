d77b9659de339367c908808541212c4d4c1df311
crazy rabbidz <hugsy@users.noreply.github.com>
2020-02-29 10:11:23 -0800

    Create gef-demo-intro.py

---
+++ gef-demo-intro.py
@@ -0,0 +1,7 @@
+print("=========================================")
+print("Welcome to the online demo version of GEF")
+print("=========================================")
+
+print()
+
+print("To start debugging, simply type: run")
