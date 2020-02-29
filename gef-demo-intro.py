fb043c1ee797d3f6e9c47a3e3922880f47191bac
crazy rabbidz <hugsy@users.noreply.github.com>
2020-02-29 10:20:57 -0800

    Update gef-demo-intro.py

--- gef-demo-intro.py
+++ gef-demo-intro.py
@@ -1,13 +1,41 @@
+import random
+
+
+def random_tip_of_the_day():
+  tips = open("/gef/tips", "r").readlines()
+  return random.choice(tips)
+
+
 print("=========================================")
 print("Welcome to the online demo version of GEF")
 print("=========================================")
 
 print()
+print("""
+ _______________________________________________________________________
+{ To start debugging, simply type: start                                }
+{ To view the help menu, simply type: gef                               }
+{ To view/modify the current GEF configuration, simply type: gef config }
+ ------------------------------------------------------------------------
+ \\
+  \\
+     __
+    /  \\
+    |  |
+    @  @
+    |  |
+    || |/
+    || ||
+    |\\_/|
+    \\___/
+""")
+print("")
 
-print("To start debugging, simply type: start")
-print("To view the help menu, simply type: gef")
-print("To view/modify the current GEF configuration, simply type: gef config")
 
 print()
 print("Happy debugging!")
 print()
+
+print("Did you know?")
+print( random_tip_of_the_day() )
+
