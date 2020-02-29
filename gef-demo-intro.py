2bdd37a2879c61a1a0b07741c7ae34f48b282463
crazy rabbidz <hugsy@users.noreply.github.com>
2020-02-29 10:15:20 -0800

    Update gef-demo-intro.py

--- gef-demo-intro.py
+++ gef-demo-intro.py
@@ -4,4 +4,10 @@ print("=========================================")
 
 print()
 
-print("To start debugging, simply type: run")
+print("To start debugging, simply type: start")
+print("To view the help menu, simply type: gef")
+print("To view/modify the current GEF configuration, simply type: gef config")
+
+print()
+print("Happy debugging!")
+print()
